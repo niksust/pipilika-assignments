3ebd9c9c8392b45fbbc3b187f3876d58f783c81f
saif-lesnar <saif_lesnar@outlook.com>
2018-09-26 07:42:22 +0600

    added new fields and features

---
+++ Task1/spiders/pickaboo.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from Task1.items import Task1Item
+
+class PickabooSpider(scrapy.Spider):
+    name = "pickaboo"
+    allowed_domains = ["pickaboo.com"]
+    start_urls = ['https://www.pickaboo.com/']
+    custom_settings = {
+       'FEED_URI' : 'tmp/pick.csv'
+    }
+    def parse(self, response):
+        urls_categories = response.xpath('//h5/a/@href').extract()
+        for url in urls_categories:
+            yield scrapy.Request(url=url, callback=self.parse_categories)
+
+
+    def parse_categories(self, response):
+        urls_items = response.xpath('//a[@class="product-image"]/@href').extract()
+        for url_indiv in urls_items:
+            yield scrapy.Request(url=url_indiv, callback=self.parse_items)
+
+        # follow next page link
+        next_page_url = response.xpath('//a[@class="next i-next" and @title="Next"]/@href').extract_first()
+        if next_page_url:
+            yield scrapy.Request(url=next_page_url, callback=self.parse_categories)
+
+    def parse_items(self, response):
+	#initialize with fields from Task1Item
+        item = Task1Item()
+	#add to items[key] in python dict structure
+        item['title'] = response.xpath('//h1[@class="product-productname"]/text()').extract_first()
+        item['product_url'] = response.url
+        list = response.xpath('//span[@itemprop="title"]/text()').extract()
+        item['category'] = ""
+        if len(list) > 1: #more than one catagories is available so number 2 is very close to required catagory
+            item['category'] = response.xpath('//span[@itemprop="title"]/text()')[1].extract()
+        item['img_url'] = response.xpath('//*[@id="owl-demo"]//img/@src').extract_first()
+        item['image_urls'] = [item['img_url']]
+        #item['image_urls'] = response.xpath('//*[@id="owl-demo"]//img/@src').extract_first() #[0].extract()
+        item['price'] = response.xpath('//span[@class="price" and @itemprop="price"]/@content').extract_first()
+        item['discounted'] = response.xpath('//strong[@style="font-size:18px;"]/text()').extract()
+        item['rating'] = response.xpath('//div[@class="mobile-pdt-rating"]//p[@class="amount-mobile"]/text()').extract()
+        item['sku'] = response.xpath('//th[contains(text(), "SKU")]/following-sibling::td/text()').extract() #Stock Keeping Unit -- a unique id
+        yield item
